0cebe39bb460c272d6ce6b670dd998df1280580b
Felix Nyamweya <57399460+fnyamweya@users.noreply.github.com>
2025-12-30 15:52:57 +0000

    fix(cors): allow localhost:5000 frontend

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,8 @@ async function bootstrap() {
   const corsOptions = {
     origin: [
       'http://localhost:3000',
+      'http://localhost:5000',
+      'http://127.0.0.1:5000',
       'http://localhost:5173',
       'http://localhost:5174',
       'https://junemobile-admin.shwecode.xyz',
